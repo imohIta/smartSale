40ca729817912f7872f1cf19f2dd2d373d23155b
Imoh Abrahams <su.imohita@gmail.com>
2017-11-26 17:41:22 +0100

    Restrict directory browsing by redirecting request to unauthorized directories to public directory'

--- application/components/controllers/index.php
+++ application/components/controllers/index.php
@@ -7,8 +7,7 @@
 define ("PATH", realpath(__DIR__));
 
 $parts = explode(DIRECTORY_SEPARATOR, PATH);
-define("PARENT_DIR", $parts[count($parts) - 2]);
-
+define("PARENT_DIR", $parts[count($parts) - 4]);
 
 $location = 'http://' . $_SERVER['HTTP_HOST'] . '/' . PARENT_DIR . '/public/';
 header('Location:' . $location);
